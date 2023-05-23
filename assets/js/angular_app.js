e87362883f759ae31e8bfc4eae8964dc0726871d
MBMishu <masumbillah1717@gmail.com>
2023-05-23 06:56:11 +0600

    add databse

--- assets/js/angular_app.js
+++ assets/js/angular_app.js
@@ -30,64 +30,35 @@ var angular_app = function () {
     });
   };
   fullHeight();
-  if ($(".skill-list").length) {
-    $(".skill-list").owlCarousel({
-      loop: true,
-      nav: false,
-      dots: true,
-      items: 3,
-      margin: 30,
-      autoplay: true,
-      smartSpeed: 100,
-      autoplayTimeout: 6000,
-      responsive: {
-        0: {
-          items: 4,
-          margin: 10,
-        },
-        460: {
-          items: 4,
-          margin: 10,
-        },
-        576: {
-          items: 6,
-          margin: 20,
-        },
-        992: {
-          items: 8,
-          margin: 30,
-        },
-      },
-    });
-  }
-  if ($(".resume-list").length) {
-    $(".resume-list").owlCarousel({
-      loop: true,
-      nav: false,
-      dots: true,
-      items: 3,
-      margin: 30,
-      autoplay: false,
-      smartSpeed: 700,
-      autoplayTimeout: 6000,
-      responsive: {
-        0: {
-          items: 1,
-          margin: 0,
-        },
-        460: {
-          items: 1,
-          margin: 0,
-        },
-        576: {
-          items: 2,
-          margin: 20,
-        },
-        992: {
-          items: 2,
-          margin: 30,
-        },
-      },
-    });
-  }
+
+  // if ($(".resume-list").length) {
+  //   $(".resume-list").owlCarousel({
+  //     loop: true,
+  //     nav: false,
+  //     dots: true,
+  //     items: 3,
+  //     margin: 30,
+  //     autoplay: false,
+  //     smartSpeed: 700,
+  //     autoplayTimeout: 6000,
+  //     responsive: {
+  //       0: {
+  //         items: 1,
+  //         margin: 0,
+  //       },
+  //       460: {
+  //         items: 1,
+  //         margin: 0,
+  //       },
+  //       576: {
+  //         items: 2,
+  //         margin: 20,
+  //       },
+  //       992: {
+  //         items: 2,
+  //         margin: 30,
+  //       },
+  //     },
+  //   });
+  // }
 };
